5932115f580274304ef5ce29b08e58045f4a06aa
Kirrrkaz <74609707+Kirrrkaz@users.noreply.github.com>
2022-04-13 13:31:09 +0300

    Add files via upload

---
+++ passgen.py
@@ -0,0 +1,26 @@
+#ver 2.0
+import itertools
+
+def split(s):
+    return [char for char in password]
+
+password = str(input('pass: '))
+tailpass = str(input('tail-pass: '))MyList = [password]
+lenght = len(password)
+
+NewPass = split(password)
+print(NewPass)
+
+def rSubset(NewPass):
+    return list(itertools.permutations(NewPass))
+    
+if __name__ == "__main__":
+    NewPass = (rSubset(NewPass))
+    '''temp = NewPass
+    print(" ".join(temp))'''
+    
+    
+for i in (NewPass):
+    for j in i:
+        print(j, end='')
+    print(tailpass)
