8710d6292aae2c59293bb7d5094069423c185fcd
Kirrrkaz <74609707+Kirrrkaz@users.noreply.github.com>
2022-04-13 20:13:13 +0300

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -5,7 +5,8 @@ def split(s):
     return [char for char in password]
 
 password = str(input('pass: '))
-tailpass = str(input('tail-pass: '))MyList = [password]
+tailpass = str(input('tail-pass: '))
+MyList = [password]
 lenght = len(password)
 
 NewPass = split(password)
@@ -16,11 +17,8 @@ def rSubset(NewPass):
     
 if __name__ == "__main__":
     NewPass = (rSubset(NewPass))
-    '''temp = NewPass
-    print(" ".join(temp))'''
     
-    
-for i in (NewPass):
+for i in (NewPass):
     for j in i:
         print(j, end='')
-    print(tailpass)
+    print(tailpass)
